462f7b5dae1828e100e539394a94b367945455db
Jeruza De Moura Cruz <jeruza.moura@xpi.com.br>
2021-11-01 09:36:06 -0300

    Implementando entidade client e address

---
+++ BlueBank/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueBank.Entities
+{
+    public abstract class Client
+	{
+        public string Name { get; set; }
+        public string CPF { get; set; }
+
+        public string Phone { get; set; }
+
+        public Guid Id { get; set; }
+
+        public Address Address { get; set; }
+
+    }
+}
