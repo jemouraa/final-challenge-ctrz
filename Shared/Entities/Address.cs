c74b7bb50a8167fd014039c9fb4ba3569686ea5b
Jeruza De Moura Cruz <jeruza.moura@xpi.com.br>
2021-11-01 09:48:20 -0300

    Implementando shared

---
+++ Shared/Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueBank.Entities
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public int Number { get; set; }
+        public string State { get; set; }
+
+    }
+}
